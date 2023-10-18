b47e57f8dff2a3764d161c5bd0f41ca86c4aa394
Raudel Valdes <rvaldesarias@RVA-MBP.local>
2023-10-18 11:56:32 -0400

    contact card completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 void main() {
   runApp(MyApp());
@@ -9,21 +12,93 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(
-          backgroundColor: Colors.teal,
-          title: Text('Demo'),
-        ),
+        backgroundColor: Colors.teal,
         body: SafeArea(
           child: Column(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
               Center(
-                  child: CircleAvatar(
-                radius: 50.0,
-                backgroundImage: AssetImage("images/avatar_raudel.jpg"),
-              )),
-              Center(child: Text("Raudel Valdes"))
+                child: CircleAvatar(
+                  radius: 50.0,
+                  backgroundImage: AssetImage('images/avatar_raudel.jpg'),
+                ),
+              ),
+              Text(
+                'Raudel Valdes',
+                style: TextStyle(
+                  fontFamily: 'Pixelify',
+                  fontSize: 40.0,
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                ),
+                textAlign: TextAlign.center,
+              ),
+              Text(
+                'SOFTWARE ENGINEER\nFlutter, Angular, Node.JS, GCP',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 15.0,
+                  letterSpacing: 2.5,
+                  fontWeight: FontWeight.bold,
+                ),
+                softWrap: true,
+                textAlign: TextAlign.center,
+              ),
+              SizedBox(
+                height: 20.0,
+                width: 200.0,
+                child: Divider(
+                  color: Colors.teal.shade100,
+                ),
+              ),
+              InkWell(
+                onTap: () => launch('tel://+150241831**'),
+                child: Card(
+                  margin:
+                      EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                  color: Colors.white,
+                  child: Padding(
+                    padding: EdgeInsets.all(5.0),
+                    child: ListTile(
+                      leading: Icon(
+                        Icons.phone,
+                        color: Colors.teal,
+                      ),
+                      title: Text(
+                        '(502) 418 - 31**',
+                        style: TextStyle(
+                          color: Colors.teal.shade900,
+                          fontSize: 15.0,
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+              InkWell(
+                onTap: () => launch("mailto:raudelvaldes**@gmail.com"),
+                child: Card(
+                  margin:
+                      EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                  color: Colors.white,
+                  child: Padding(
+                    padding: EdgeInsets.all(5.0),
+                    child: ListTile(
+                      leading: Icon(
+                        Icons.email,
+                        color: Colors.teal,
+                      ),
+                      title: Text(
+                        'raudelvaldes**@gmail.com',
+                        style: TextStyle(
+                          color: Colors.teal.shade900,
+                          fontSize: 15.0,
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              )
             ],
           ),
         ),
